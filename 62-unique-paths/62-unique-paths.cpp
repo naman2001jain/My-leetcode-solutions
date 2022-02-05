b175fc33b48a2da1d3129fb1f4b61bb12b3ee0c9
Naman jain <62708894+naman2001jain@users.noreply.github.com>
2022-02-05 11:38:31 +0530

    Time: 3 ms (28.96%), Space: 5.9 MB (70.09%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    
+    int uniquePaths(int m, int n) {
+        int dp[105][105];
+        for(int i=0;i<105;i++){
+            for(int j=0;j<105;j++){
+                if(i==0 || j==0){
+                    dp[i][j] = 1;
+                    continue;
+                }
+                dp[i][j] = -1;
+            }
+        }
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
