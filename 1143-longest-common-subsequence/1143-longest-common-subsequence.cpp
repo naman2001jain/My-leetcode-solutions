56487940327d19506f7c0b32e76eb1ae34ce6456
Naman jain <62708894+naman2001jain@users.noreply.github.com>
2022-02-16 20:19:13 +0530

    Time: 48 ms (21.30%), Space: 10.9 MB (56.67%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -5,25 +5,25 @@ public:
         if(m==0 || n==0){
             return 0;
         }
-        if(dp[m][n] != -1){
+        
+        if(dp[m][n]!=-1){
             return dp[m][n];
         }
         if(text1[m-1] == text2[n-1]){
-            return dp[m][n] = 1 + lcs(text1, text2, m-1, n-1);
+            return dp[m][n] = 1+lcs(text1, text2, m-1,n-1);
         }else{
-            return dp[m][n] = max(lcs(text1, text2, m-1,n), lcs(text1, text2, m,n-1));
+            return dp[m][n] = max(lcs(text1, text2, m-1, n), lcs(text1, text2, m, n-1));
         }
     }
     
     int longestCommonSubsequence(string text1, string text2) {
         int m = text1.length();
         int n = text2.length();
-        for(int i=0;i<1005;i++){
-            for(int j=0;j<1005;j++){
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
                 dp[i][j] = -1;
             }
         }
-        
-        return lcs(text1, text2,m, n);
+        return lcs(text1, text2, m, n);
     }
 };
