e40a900a0af315d4e578783c905686b87b1eb973
Naman jain <62708894+naman2001jain@users.noreply.github.com>
2022-03-26 16:09:45 +0530

    Time: 1773 ms (5.47%), Space: 9 MB (78.50%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int count=0;
-    void rec(vector<int> &nums, int n, int sum, const int &target){
-        
-        
+    int rec(vector<int> &nums, int n, int target){
         
         if(n<0){
-            if(target==sum)
-                count++;
+            if(target==0){
+                return 1;
+            }
+        }else{
+            //recursion
+        int res = 0;
+    
+        res += rec(nums, n-1, target - nums[n]) + rec(nums, n-1, target + nums[n]);
+        return res;
         }
-        else{
-            rec(nums, n-1, sum + nums[n], target);
-            rec(nums, n-1, sum-nums[n], target);
-        }
+        
+        return 0;
         
     }
     
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-        int sum=0;
-        rec(nums, n-1, sum, target);
-        return count;
+        return rec(nums, n-1, target);
     }
 };
