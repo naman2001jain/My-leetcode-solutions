997a4af1c946ab3efc7c0fe7e61898d8ee1e9fb3
Naman jain <62708894+naman2001jain@users.noreply.github.com>
2022-03-30 15:57:44 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (80.11%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int dp[35];
+    
+        
+    int fib(int n) {
+        if(n==0 || n==1){
+            return n;
+        }
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+};
