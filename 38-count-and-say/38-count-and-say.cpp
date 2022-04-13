c0a233b6817153f6485d93cd20048e586bce13a4
Naman jain <62708894+naman2001jain@users.noreply.github.com>
2022-04-13 10:11:53 +0530

    Time: 12 ms (48.40%), Space: 6.6 MB (57.64%) - LeetHub

--- 38-count-and-say/38-count-and-say.cpp
+++ 38-count-and-say/38-count-and-say.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
-    string say(string s){
+    string countAndSay(int n)
+    {
+        // base case..
+        if (n == 1)
+            return "1";
         
-        string res = "";
-        int x=1;
-        for(int i=0;i<s.length();i++){
-            if(s[i]==s[i+1]){
-                x++;
-            }else{
-                res += to_string(x) + s[i];
-                x=1;
+        string s = countAndSay(n - 1);
+
+        int count = 1;
+        string temp = "";
+
+        for (int i = 0; i < s.size(); i++) {   
+             if (s[i] == s[i + 1])  count++;
+             else{
+                temp +=  to_string(count) + s[i] ;
+                count = 1;
             }
         }
-        
-        return res;
-    }
-    
-    string countAndSay(int n) {
-        string res = "";
-        if(n==1){
-            res =  "1";
-            return res;
-        }
-        if(n==2){
-            return "11";
-        }
-        res = "21";
-        for(int i=3;i<n;i++){
-            res = say(res);
-        }
-        return res;
+
+        return temp;
     }
 };
